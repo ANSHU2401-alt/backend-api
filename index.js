d9d26104f454594aa33ecb57f7257e3721550c43
anshu <anshucengage020306@gmail.com>
2026-06-13 20:50:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const cors = require("cors");
 app.use(cors({
   origin: [
@@ -13,36 +13,36 @@ app.use(cors({
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log("Server running on port", port)
-})
-app.use(express.static("public"));
-const userModel=require('./userModel.js')
-const userModel_contact=require('./usermodel_contact.js')
-const userModel_feedback=require('./usermodel_feedback.js')
+
+const userModel = require('./userModel.js')
+const userModel_contact = require('./usermodel_contact.js')
+const userModel_feedback = require('./usermodel_feedback.js')
 const jwt = require("jsonwebtoken");
 var validateDate = require("validate-date");
 const cookieParser = require("cookie-parser");
+
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   const token = req.cookies.token;
 
-  if(!token){
+  if (!token) {
     res.send('Hello World!')
   }
-  else{
+  else {
     try {
-    const data = jwt.verify(token, "secretkey123");
-    return res.redirect(`/${data.name}`);
-  } catch (err) {
-    return res.send("Session expired, login again");
-  }
+      const data = jwt.verify(token, "secretkey123");
+      return res.redirect(`/${data.name}`);
+    } catch (err) {
+      return res.send("Session expired, login again");
+    }
   }
 })
+
 app.post("/Contact", async (req, res) => {
 
   const user = await userModel.findOne({
@@ -51,44 +51,36 @@ app.post("/Contact", async (req, res) => {
   });
 
   if (!user) {
-    return res.render(
-      "notcontactuser"
-    );
+    return res.render("notcontactuser");
   }
 
   await userModel_contact.create({
-  Name: req.body.Name,
-  Email: req.body.Email,
-  Username: req.body.Username,
-  Comment: req.body.comment || req.body.Comment
-});
+    Name: req.body.Name,
+    Email: req.body.Email,
+    Username: req.body.Username,
+    Comment: req.body.comment || req.body.Comment
+  });
 
   res.render("query_donw")
 });
 
-app.post("/create",async (req,res)=>{
-const [year, month, day] = req.body.DOB.split("-").map(Number);
+app.post("/create", async (req, res) => {
+  const [year, month, day] = req.body.DOB.split("-").map(Number);
 
-if (
-    year < 1900 ||
-    year > new Date().getFullYear()
-) {
+  if (year < 1900 || year > new Date().getFullYear()) {
     return res.render("dobwrong");
-}
-    else{
-        if(req.body.Password!=req.body.ConfirmPassword){
-        res.send(`Write again`)
+  }
+  else {
+    if (req.body.Password != req.body.ConfirmPassword) {
+      res.send(`Write again`)
     }
-    else{
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hash = await bcrypt.hash(req.body.Password, salt);
-    let user_Email = await userModel.findOne(
-        { Email: req.body.Email },
-    );
-    let user_name = await userModel.findOne(
-        { Name: req.body.Name },
-    );
-    if (user_Email) {
+    else {
+      const salt = await bcrypt.genSalt(saltRounds);
+      const hash = await bcrypt.hash(req.body.Password, salt);
+      let user_Email = await userModel.findOne({ Email: req.body.Email });
+      let user_name = await userModel.findOne({ Name: req.body.Name });
+      
+      if (user_Email) {
         res.send(`
     <!DOCTYPE html>
     <html>
@@ -101,8 +93,8 @@ if (
     </body>
     </html>
   `);
-}
-    else if (user_name) {
+      }
+      else if (user_name) {
         res.send(`
     <!DOCTYPE html>
     <html>
@@ -115,18 +107,18 @@ if (
     </body>
     </html>
   `);
-}
-    else{
-    let v=await userModel.create({
-    Name:req.body.Name,
-    Email:req.body.Email,
-    Password: hash,
-    DOB:req.body.DOB
-    })
+      }
+      else {
+        let v = await userModel.create({
+          Name: req.body.Name,
+          Email: req.body.Email,
+          Password: hash,
+          DOB: req.body.DOB
+        })
         res.render('signed_successfull')
+      }
     }
-}
-    }
+  }
 })
 
 app.post("/payment-log", (req, res) => {
@@ -161,7 +153,7 @@ app.post("/login", async (req, res) => {
     user.lastLogin = today;
   }
 
-  await user.save(); 
+  await user.save();
 
   const token = jwt.sign(
     {
@@ -172,188 +164,21 @@ app.post("/login", async (req, res) => {
     { expiresIn: "1d" }
   );
 
- res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,       
-  sameSite: "none",    
-  maxAge: 24 * 60 * 60 * 1000,
-  path: "/"
-});
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+    path: "/"
+  });
 
   return res.json({
     success: true,
     username: user.Name,
-    streak: user.streak   
+    streak: user.streak
   });
 });
-// app.use(express.static("public"));
-// const userModel=require('./userModel.js')
-// const userModel_contact=require('./usermodel_contact.js')
-// const userModel_feedback=require('./usermodel_feedback.js')
-// const jwt = require("jsonwebtoken");
-// var validateDate = require("validate-date");
-// const cookieParser = require("cookie-parser");
-// app.use(cookieParser());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.set("view engine", "ejs");
 
-// app.get('/', (req, res) => {
-//   const token = req.cookies.token;
-
-//   if(!token){
-//     res.send('Hello World!')
-//   }
-//   else{
-//     try {
-//     const data = jwt.verify(token, "secretkey123");
-//     return res.redirect(`/${data.name}`);
-//   } catch (err) {
-//     return res.send("Session expired, login again");
-//   }
-//   }
-// })
-// app.post("/Contact", async (req, res) => {
-
-//   const user = await userModel.findOne({
-//     Name: req.body.Username,
-//     Email: req.body.Email
-//   });
-
-//   if (!user) {
-//     return res.render(
-//       "notcontactuser"
-//     );
-//   }
-
-//   await userModel_contact.create({
-//   Name: req.body.Name,
-//   Email: req.body.Email,
-//   Username: req.body.Username,
-//   Comment: req.body.comment || req.body.Comment
-// });
-
-//   res.render("query_donw")
-// });
-
-// app.post("/create",async (req,res)=>{
-// const [year, month, day] = req.body.DOB.split("-").map(Number);
-
-// if (
-//     year < 1900 ||
-//     year > new Date().getFullYear()
-// ) {
-//     return res.render("dobwrong");
-// }
-//     else{
-//         if(req.body.Password!=req.body.ConfirmPassword){
-//         res.send(`Write again`)
-//     }
-//     else{
-//     const salt = await bcrypt.genSalt(saltRounds);
-//     const hash = await bcrypt.hash(req.body.Password, salt);
-//     let user_Email = await userModel.findOne(
-//         { Email: req.body.Email },
-//     );
-//     let user_name = await userModel.findOne(
-//         { Name: req.body.Name },
-//     );
-//     if (user_Email) {
-//         res.send(`
-//     <!DOCTYPE html>
-//     <html>
-//     <head>
-//       <script src="https://cdn.tailwindcss.com"></script>
-//     </head>
-//     <body class="bg-black text-white flex items-center justify-center h-screen space-x-2">
-//       <h1 class="text-4xl md:text-3xl font-bold">Person already exists </h1>
-//       <div class="bg-emerald-400 hover:bg-emerald-500 px-2 py-2 rounded-md"><a href="https://backend-api-p3b2.onrender.com/login" class="text-3xl md:text-xl">Go back</a></div>
-//     </body>
-//     </html>
-//   `);
-// }
-//     else if (user_name) {
-//         res.send(`
-//     <!DOCTYPE html>
-//     <html>
-//     <head>
-//       <script src="https://cdn.tailwindcss.com"></script>
-//     </head>
-//     <body class="bg-black text-white flex items-center justify-center h-screen space-x-2">
-//       <h1 class="text-4xl md:text-3xl font-bold">Person already exists </h1>
-//       <div class="bg-emerald-400 hover:bg-emerald-500 px-2 py-2 rounded-md"><a href="https://backend-api-p3b2.onrender.com/login" class="text-3xl md:text-xl">Go back</a></div>
-//     </body>
-//     </html>
-//   `);
-// }
-//     else{
-//     let v=await userModel.create({
-//     Name:req.body.Name,
-//     Email:req.body.Email,
-//     Password: hash,
-//     DOB:req.body.DOB
-//     })
-//         res.render('signed_successfull')
-//     }
-// }
-//     }
-// })
-
-// app.post("/payment-log", (req, res) => {
-//   console.log("Payment initiated:", req.body);
-//   res.send("ok");
-// });
-
-// app.post("/login", async (req, res) => {
-//   console.log("LOGIN REQUEST BODY:", req.body);
-
-//   let name = req.body.Name;
-//   let password = req.body.Password;
-
-//   let user = await userModel.findOne({ Name: name });
-
-//   if (!user) {
-//     return res.json({ success: false });
-//   }
-
-//   let isMatch = await bcrypt.compare(password, user.Password);
-
-//   if (!isMatch) {
-//     return res.json({ success: false });
-//   }
-
-//   const today = new Date().toDateString();
-
-//   if (!user.streak) user.streak = 0;
-
-//   if (user.lastLogin !== today) {
-//     user.streak += 1;
-//     user.lastLogin = today;
-//   }
-
-//   await user.save(); 
-
-//   const token = jwt.sign(
-//     {
-//       id: user._id,
-//       name: user.Name
-//     },
-//     "secretkey123",
-//     { expiresIn: "1d" }
-//   );
-
-//   res.cookie("token", token, {
-//     httpOnly: true,
-//     maxAge: 24 * 60 * 60 * 1000,
-//     path: "/"
-//   });
-
-//   return res.json({
-//     success: true,
-//     username: user.Name,
-//     streak: user.streak   
-//   });
-// });
 app.get("/checklogin", async (req, res) => {
   const token = req.cookies.token;
 
@@ -363,7 +188,6 @@ app.get("/checklogin", async (req, res) => {
 
   try {
     const data = jwt.verify(token, "secretkey123");
-
     const user = await userModel.findById(data.id);
 
     return res.json({
@@ -385,4 +209,6 @@ app.post("/feedback", async (req, res) => {
   res.render('feedback')
 });
 
-});
+app.listen(port, () => {
+  console.log("Server running on port", port)
+})
